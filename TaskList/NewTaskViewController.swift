8c6fe558d8b0e0073ec1aa37a3e3da3757ff3cc7
soloexploder <soloexploder@gmail.com>
2022-05-30 17:44:00 +0300

    +task VC

---
+++ TaskList/NewTaskViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NewTaskViewController.swift
+//  TaskList
+//
+//  Created by Даниил Хантуров on 14.05.2022.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
